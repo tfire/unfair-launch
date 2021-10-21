4975cabef9ed49a3b0a66427cd6eb9e69ef56ebc
Tyler Feuerstein <tylerfeuerstein@gmail.com>
2021-10-20 21:51:56 -0400

    alert message formatting/delivery

--- discordbot.py
+++ discordbot.py
@@ -10,13 +10,34 @@ import discord
 from discord.ext import commands
 from discord.ext.commands import bot
 
+def format_liquidity_pool_created_alert(ticker, underlying, stats):
+    return f"""
+@here
+```
+Liquidity Pool Created: {ticker}/{underlying}
+
+{ticker} mentioned in {stats["all_tweets"]} tweets
+
+Total favorites: {stats["all_favorites"]}
+Most favorites: {stats["max_favorites"]} twitter.com/i/status/{stats["max_fav_id"]}
+
+Total RTs: {stats["all_retweets"]}
+Most RTs: {stats["max_retweets"]} twitter.com/i/status/{stats["max_rt_id"]}
+```
+:eyes: to watch liquidity
+:white_check_mark: to automate entry
+"""
+
+
 class UnfairLaunchBot(bot.BotBase, discord.Client):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.channel = None
         asyncio.run_coroutine_threadsafe(self.sushi_monitor(), self.loop)
 
     async def on_ready(self):
         print(self.user)
+        self.channel = self.get_channel(discord_keys.UNFAIR_CHANNEL)
 
     async def sushi_monitor(self):
         await self.wait_until_ready()
@@ -42,9 +63,20 @@ class UnfairLaunchBot(bot.BotBase, discord.Client):
 
     async def handle(self, event):
         print(event)
-
+        decoded = utils.decode_PairCreated(event)
+        print(decoded)
+        token = utils.get_main_token_for_pair(decoded[0], decoded[1])
+        ticker = utils.get_ticker_at_erc20(token)
+        tweets, stats = utils.get_tweets("$" + ticker)
+        self.channel.send(format_liquidity_pool_created_alert(ticker, "USDC", stats))
+        
 
 
 b = UnfairLaunchBot(command_prefix="!")
+
+@b.command()
+async def test(ctx):
+    await ctx.send("hello")
+
 b.run(discord_keys.DISCORD_BOT_KEY)
 
